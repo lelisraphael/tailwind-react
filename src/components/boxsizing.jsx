b9629b38988c178c8e40ffc10ba11ee5fcb2ac6f
lelisraphael <g5raphael@hotmail.com>
2022-03-08 19:45:30 -0300

    border-box border-content container

---
+++ src/components/boxsizing.jsx
@@ -0,0 +1,18 @@
+const BoxSizing = () => {
+
+    return (
+        <>
+            <div className="container border-2 border-purple-900 bg-green-200 m-4 border-box p-10">
+                <p>Border Box</p>
+                *Respeita a largura total com paddings e margins
+            </div>
+
+            <div className="container bg-blue-200 border-2 border-purple-900 m-4 box-content p-10">
+                <p>Border Content</p>
+                *soma os paddings e margins
+            </div>
+        </>
+    )
+
+}
+export default BoxSizing
---
+++ src/components/container.jsx
@@ -0,0 +1,10 @@
+const Container = () => {
+    return (
+        <>
+            <div className="container bg-slate-500 mx-auto p-4 my-4 text-white border-2">
+                Container
+            </div>
+        </>
+    )
+}
+export default Container
