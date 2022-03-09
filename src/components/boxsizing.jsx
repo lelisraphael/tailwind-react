9ceddfa54fb89696ad0281ad8fe6c331175f83ac
lelisraphael <g5raphael@hotmail.com>
2022-03-08 21:02:26 -0300

    box sizing

--- src/components/boxsizing.jsx
+++ src/components/boxsizing.jsx
@@ -1,7 +1,7 @@
 const BoxSizing = () => {
 
     return (
-        <>
+        <div className="container mx-auto">
             <div className="container border-2 border-purple-900 bg-green-200 m-4 border-box p-10">
                 <p>Border Box</p>
                 *Respeita a largura total com paddings e margins
@@ -11,7 +11,7 @@ const BoxSizing = () => {
                 <p>Border Content</p>
                 *soma os paddings e margins
             </div>
-        </>
+        </div>
     )
 
 }
