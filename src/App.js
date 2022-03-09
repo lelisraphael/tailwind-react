6fa90836a85c191ae0638048ddcc9c27fcff50b8
lelisraphael <g5raphael@hotmail.com>
2022-03-08 21:03:50 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Container from './components/container';
 import BoxSizing from './components/boxsizing';
+import Display from './components/display';
+import Overflow from './components/overflow'
+import Positions from './components/positions';
+import Zindex from './components/zindex';
+import Direction from './components/flexbox/directions';
 
 function App() {
   return (
     <>
-      <Container />
+      {/* <Container />
       <BoxSizing />
+      <Display />
+      <Overflow />
+      <Positions />
+      <Zindex /> */}
+      <Direction/>
+
     </>
   );
 }
