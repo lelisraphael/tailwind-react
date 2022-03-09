966be22f60cdafd29cc7f5e4c67c4547a94433e4
lelisraphael <g5raphael@hotmail.com>
2022-03-08 21:03:40 -0300

    positions

---
+++ src/components/positions.jsx
@@ -0,0 +1,12 @@
+const Positions = () => {
+
+    return (
+        <div className="relative bg-pink-500">
+            Pai do Absolute
+            <div className="absolute top-0 right-0 bg-pink-100">
+                Absolute com elemento pai
+            </div>
+        </div >
+    )
+}
+export default Positions;
