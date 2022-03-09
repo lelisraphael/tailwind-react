cfcdcb7dd96e737f763d35427254244907685d24
lelisraphael <g5raphael@hotmail.com>
2022-03-09 09:35:57 -0300

    order

---
+++ src/components/flexbox/order.jsx
@@ -0,0 +1,20 @@
+const Order = () => {
+
+    return (
+        <>
+
+            <div className="flex flex-wrap bg-purple-300">
+                <div className="bg-purple-400 p-6 m-3 order-last">1</div>
+                <div className="bg-purple-500 p-6 m-3 order-5">2</div>
+                <div className="bg-purple-600 p-6 m-3 order-4">3</div>
+                <div className="bg-purple-700 p-6 m-3 order-3">4</div>
+                <div className="bg-purple-800 p-6 m-3 order-2">5</div>
+                <div className="bg-purple-900 p-6 m-3 order-first">6</div>
+            </div>
+
+
+        </>
+    )
+
+}
+export default Order
